8b4770af4d2ca7cf37e9a4bacadd10b96f0dd93a
Jairo Esteban Martínez Portillo <61020858+JairoBoss@users.noreply.github.com>
2021-02-27 01:06:17 -0600

    .

---
+++ CaseProblem1/Event.java
@@ -0,0 +1,11 @@
+package CaseProblem1;
+
+public class Event {
+    public static void main(String[] args) {
+        EventDemo fiesta = new EventDemo();
+        fiesta.setNumeroDeTelefono();
+        System.out.println(fiesta.getNumeroDeTelefono());
+        System.out.println(fiesta.getEvento());
+
+    }
+}
